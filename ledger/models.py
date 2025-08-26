6d8b878c0e5db3fe806243a5324a0de0bfe16898
OneArmedofLepanto <high.kick.jake@gmail.com>
2025-08-26 14:47:02 +0900

    [fix#14] event의 description 필드에 null True 추가

--- ledger/models.py
+++ ledger/models.py
@@ -24,7 +24,7 @@ class Event(models.Model):
     name = models.CharField(max_length=100)
     start_date = models.DateField()
     end_date = models.DateField()
-    description = models.TextField()
+    description = models.TextField(null=True, blank=True)
     budget = models.IntegerField()
 
 
