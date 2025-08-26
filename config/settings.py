d622f0c3ca7a4159049a5bd6fbcf769a43b774b7
OneArmedofLepanto <high.kick.jake@gmail.com>
2025-08-27 00:19:01 +0900

    [feature#8]코드 수정, faiss 생성

--- config/settings.py
+++ config/settings.py
@@ -28,7 +28,7 @@ load_dotenv()
 SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", "your-default-secret-key")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.getenv("DEBUG")
 
 ALLOWED_HOSTS = ["127.0.0.1", "localhost", "54.206.122.170"]
 
